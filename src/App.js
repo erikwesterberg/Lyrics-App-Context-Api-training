b6a2103522cd8b5cb2904c4888978cd022e7b038
erka <erik.westerberg@hotmail.se>
2019-09-23 12:50:04 +0200

    wraps app component wit provider so props are available to all childs

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React from "react";
 import Navbar from "./components/layout/Navbar";
-import Index from './components/layout/Index'
+import Index from "./components/layout/Index";
+import { Provider } from "./context";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 
 const App = () => {
   return (
-    <Router>
-      <Navbar />
-      <div className="container">
-        <Switch>
-          <Route exact path="/" component={Index}></Route>
-        </Switch>
-      </div>
-    </Router>
+    <Provider>
+      <Router>
+        <Navbar />
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={Index}></Route>
+          </Switch>
+        </div>
+      </Router>
+    </Provider>
   );
 };
 
