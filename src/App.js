e69abb91fc2db508cbd138926181be86e316889d
erka <erik.westerberg@hotmail.se>
2019-09-23 12:32:31 +0200

    installs broswer router and wraps my new index layout file inside of a route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import React from 'react';
-import Navbar from './components/layout/Navbar';
-import './App.css';
+import React from "react";
+import Navbar from "./components/layout/Navbar";
+import Index from './components/layout/Index'
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
 
 const App = () => {
   return (
-    
-    <Navbar/>
-  
+    <Router>
+      <Navbar />
+      <div className="container">
+        <Switch>
+          <Route exact path="/" component={Index}></Route>
+        </Switch>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
