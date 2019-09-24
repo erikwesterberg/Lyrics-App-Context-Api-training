774468c36931a84bc00cce4c39070413648c09d0
erka <erik.westerberg@hotmail.se>
2019-09-24 10:24:40 +0200

    refactores context component

--- src/context.js
+++ src/context.js
@@ -1,44 +1,47 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-const Context = React.createContext();
+export const Context = React.createContext();
 
-const reducer = (state, action) => {
-  switch(action.type){
-    case "SEARCH_TRACKS":
-    return {
-      ...state,
-      track_list: action.payload,
-      heading: "Search Result"
-    };
-    default:
-      return state;
-  }
-}
+// const reducer = (state, action) => {
+//   switch (action.type) {
+//     case 'SEARCH_TRACKS':
+//       return {
+//         ...state,
+//         track_list: action.payload,
+//         heading: 'Search Results'
+//       };
+//     default:
+//       return state;
+//   }
+// };
 
-export class Provider extends Component {
-  state = {
+export function ContextController({ children }) {
+  let intialState = {
     track_list: [],
-    heading: "Top 10 tracks.",
-    dispatch: action => this.setState(state => reducer(state, action))
+    heading: ""
+    // dispatch: action => this.setState(state => reducer(state, action))
   };
 
-  componentDidMount() {
+  const [state, setState] = useState(intialState);
+
+  useEffect(() => {
     axios
       .get(
         `https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=top&page=1&page_size=10&country=us&f_has_lyrics=1&apikey=${process.env.REACT_APP_MM_KEY}`
       )
+      
       .then(res => {
-      this.setState({track_list: res.data.message.body.track_list})})
+        // console.log(res.data);
+        setState({
+          track_list: res.data.message.body.track_list,
+          heading: "Top 10 Tracks"
+        });
+      })
       .catch(err => console.log(err));
-  }
-  render() {
-    return (
-      <Context.Provider value={this.state}>
-        {this.props.children}
-      </Context.Provider>
-    );
-  }
-}
+  }, []);
 
-export const Consumer = Context.Consumer;
+  return (
+    <Context.Provider value={[state, setState]}>{children}</Context.Provider>
+  );
+}
