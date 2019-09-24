b013eb8376e43c7d4e3fbfd7fbf00fe298fe3551
erka <erik.westerberg@hotmail.se>
2019-09-24 10:12:16 +0200

    refactores tracks component to a func coomponent

--- src/components/tracks/Tracks.js
+++ src/components/tracks/Tracks.js
@@ -1,33 +1,25 @@
-import React, { Component } from 'react';
-import { Consumer } from '../../context';
-import Spinner from '../layout/Spinner'
-import Track from './Track';
+import React, { useContext } from "react";
+import { Context } from "../../context";
+import Spinner from "../layout/Spinner";
+import Track from "./Track";
 
-class Tracks extends Component {
-  render() {
+const Tracks = () => {
+  const [state] = useContext(Context);
+  const { track_list, heading } = state;
+
+  if (track_list === undefined || track_list.length === 0) {
+    return <Spinner />;
+  } else {
     return (
-      <Consumer>
-        {value => {
-        const { track_list, heading } = value;
-          if(track_list === undefined || track_list.length === 0 ) {
-            return <Spinner />
-          } else {
-            return (
-              <>
-              <h3 className="text-center mb-4">{heading}</h3>
-              <div className="row">
-                {track_list.map(item => (
-                  <Track key={item.track.track_id} track={item.track}/>
-                ))}
-              </div>
-              </>
-            )
-            
-          }
-        }}
-      </Consumer>
-    )
+      <>
+        <h3 className="text-center mb-4">{heading}</h3>
+        <div className="row">
+          {track_list.map(item => (
+            <Track key={item.track.track_id} track={item.track} />
+          ))}
+        </div>
+      </>
+    );
   }
-}
-
+};
 export default Tracks;
